d54e3ae131360a44504280c8f3676bf5b4562517
Hector Campos <85914930+hectorudg@users.noreply.github.com>
2024-06-10 17:59:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    numero = random.randint(1, 10)
+    if numero > 0:
+    return "El número es positivo"
+    elif numero == 0:
+    rerturn("El número es cero")
+    else:
     return 'Hello World!2'
     
 if __name__ == '__main__':
