15092dd8f00086aa0f64ca7731910f67e37531a5
Hector Campos <85914930+hectorudg@users.noreply.github.com>
2024-06-10 17:53:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def case_example():
-    switch = {
-        1: "HelloW",
-        2: "bye"
-    }
-    return switch.get(1, default_case)
+def hello_world():
+    return 'Hello World!2'
     
 if __name__ == '__main__':
     app.run()
