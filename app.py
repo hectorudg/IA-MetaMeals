8d18156a17f0d18591ade2b72d6fcade966a7a64
Hector Campos <85914930+hectorudg@users.noreply.github.com>
2024-06-10 17:51:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def case_example(value):
-    def case_one():
-        return "Caso 1: valor es 1"
-
-    def case_two():
-        return "Caso 2: valor es 2"
-
-    def default_case():
-        return "Caso por defecto: valor no reconocido"
-
+def case_example():
     switch = {
-        1: case_one,
-        2: case_two
+        1: "HelloW",
+        2: "bye"
     }
-
-    return switch.get(1, default_case)()
-case_example(1)
-
-
+    return switch.get(1, default_case)
+    
 if __name__ == '__main__':
     app.run()
